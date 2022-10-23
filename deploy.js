b8f3edfef16a2e4180f6f97be45460637b16e4ff
Sea Grapes <natuloid@gmail.com>
2022-10-23 16:48:31 -0700

    c

--- deploy.js
+++ deploy.js
@@ -6,30 +6,4 @@ const token = process.env.token;
 const fs = require('node:fs');
 
 export function deploy() {
-  const commands = [];
-  const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
-
-  for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    commands.push(command.data.toJSON());
-  }
-
-  const rest = new REST({ version: '10' }).setToken(token);
-
-  (async () => {
-    try {
-      console.log(`Started refreshing ${commands.length} application (/) commands.`);
-
-      const data = await rest.put(
-        Routes.applicationGuildCommands(clientId, guildId),
-        { body: commands },
-      );
-
-      console.log(`Successfully reloaded ${data.length} application (/) commands.`);
-    } catch (error) {
-
-      console.error(error);
-    }
-  })();
 }
